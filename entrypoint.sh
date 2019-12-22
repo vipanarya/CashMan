b55105b337be87deb34eeb7e812b265b0ee3ee2e
vipanarya <vipankumararya@yahoo.com>
2019-12-22 19:12:33 +1100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 export JAVA_AGENT="-javaagent:newrelic.jar"
 
-wget -o newrelic.jar https://download.newrelic.com/newrelic/java-agent/newrelic-agent/current/newrelic.jar
-wget -o newrelic.yml https://download.newrelic.com/newrelic/java-agent/newrelic-agent/current/newrelic.yml
+wget https://download.newrelic.com/newrelic/java-agent/newrelic-agent/current/newrelic.jar
+wget https://download.newrelic.com/newrelic/java-agent/newrelic-agent/current/newrelic.yml
 
 
 java ${JAVA_AGENT} -jar cashman-rest-service-0.1.0.jar
